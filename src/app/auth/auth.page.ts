6de182f268f9b537947f7cad1661e5c1ef9a38a9
Mia Morticia <miamorticia@Mias-MacBook-Pro.local>
2021-01-22 10:04:58 -0700

    adding form

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -37,13 +37,22 @@ export class AuthPage implements OnInit {
       });
   }
 
-  onSwitchAuthMode(){
+  onSwitchAuthMode() {
     this.isLogin = !this.isLogin;
   }
-  
+
   onSubmit(form: NgForm) {
-    console.log(form);
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+    console.log(email, password);
+
+    if (this.isLogin) {
+      // Send a request to login servers
+    } else {
+      // Send a request to signup servers
+    }
   }
-
-
 }
