e5394b2f76a92c466b22b2090e65b1ab93476e3c
Mia Morticia <miamorticia@Mias-MacBook-Pro.local>
2021-01-22 16:45:23 -0700

    date controls & Validating form

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -12,21 +12,27 @@ export class PlacesService {
       'Denver Penthouse',
       'In the center of everything 5280',
       'https://i2.wp.com/www.denverpost.com/wp-content/uploads/2017/04/four-seasons-4400-2-6-17-living-wide-web.jpg?sharp=10&vib=20&w=1200',
-      149.99
+      99.99,
+      new Date('2021-01-01'),
+      new Date('2021-12-31')
     ),
     new Place(
       'p2',
       "A Highlands Getaway",
       'Bungalow in heart of the Highlands, Denver',
       'https://a0.muscache.com/im/pictures/b71eccfd-a7f6-4db8-a24b-09c5ccc2c610.jpg?im_w=960',
-      189.99
+      59.99,
+      new Date('2021-01-01'),
+      new Date('2021-12-31')
     ),
     new Place(
       'p3',
       'Close to Campus',
       'CU soon!',
       'https://www.colorado.edu/studentaffairs/sites/default/files/styles/medium/public/article-image/off-campus_housing_2.jpg?itok=C9Hw9TRb',
-      99.99
+      49.99,
+      new Date('2021-01-01'),
+      new Date('2021-12-31')
     )
   ];
 
