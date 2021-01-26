435b81e4e89612cba91864d34c737bea99164ed6
Mia Morticia <miamorticia@Mias-MacBook-Pro.local>
2021-01-26 09:49:06 -0700

    map add

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  googleMapsAPIKey: 'AIzaSyDfsu2bk68Vz3EQUU4M6kALtthZsefsi0Y'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  googleMapsAPIKey: 'AIzaSyDfsu2bk68Vz3EQUU4M6kALtthZsefsi0Y'
 };
 
 /*
