46df3cd0b2eb54a0359160062873bc141ef3f8e0
Mia Morticia <miamorticia@Mias-MacBook-Pro.local>
2021-01-27 10:14:21 -0700

    geolocate logic

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# iLoftz App
+# Loftz App
 
 
 This app will be a hybrid AirBnB / Yelp app where users can create accounts, book and rate properties. 
--- src/app/shared/pickers/location-picker/location-picker.component.ts
+++ src/app/shared/pickers/location-picker/location-picker.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { 
+  ModalController, 
+  ActionSheetController, 
+  AlertController
+} from '@ionic/angular';
 import { HttpClient } from '@angular/common/http';
 import { map, switchMap } from 'rxjs/operators';
 import { of } from 'rxjs';
+import { Plugins, Capacitor } from "@capacitor/core";
 
 import { MapModalComponent } from '../../map-modal/map-modal.component';
 import { environment } from '../../../../environments/environment';
-import { PlaceLocation } from "../../../places/location.model";
+import { Coordinates, PlaceLocation } from "../../../places/location.model";
 
 @Component({
   selector: 'app-location-picker',
@@ -18,50 +23,112 @@ export class LocationPickerComponent implements OnInit {
   selectedLocationImage: string;
   isLoading = false;
 
-  constructor(private modalCtrl: ModalController, private http: HttpClient) {}
+  constructor(
+    private modalCtrl: ModalController, 
+    private http: HttpClient,
+    private actionSheetCtrl: ActionSheetController,
+    private alertCtrl: AlertController
+    ) {}
 
-  ngOnInit() {
-    
-  }
+  ngOnInit() {}
 
   onPickLocation() {
-    this.modalCtrl.create({ component: MapModalComponent }).then(modalEl => {
-      modalEl.onDidDismiss().then(modalData => {
-        if (!modalData.data) {
-         // console.log(modalData.data);
-          return;
-        }
-        const pickedLocation: PlaceLocation = {
-          lat: modalData.data.lat,
-          lng: modalData.data.lng,
-          address: null,
-          staticMapImageUrl: null
-        };
-        this.isLoading = true;
-        this.getAddress(modalData.data.lat, modalData.data.lng)
-        .pipe(
-          switchMap(address => {
-            pickedLocation.address = address;
-            return of(
-              this.getMapImage(pickedLocation.lat, pickedLocation.lng, 14)
-            )
-          })
-          ).subscribe(staticMapImageUrl => {
-            pickedLocation.staticMapImageUrl = staticMapImageUrl;
-            this.selectedLocationImage = staticMapImageUrl;
-            this.isLoading = false;
-            this.locationPick.emit(pickedLocation);
-          });
-        // .subscribe(
-        //   address => {
-         //    console.log(address);
-        //   }
-        // );
+     this.actionSheetCtrl
+       .create({
+         header: 'Please Choose',
+         buttons: [
+           {
+             text: 'Auto-Locate',
+            handler: () => {
+              this.locateUser();
+            }
+          },
+           {
+             text: 'Pick on Map',
+             handler: () => {
+               this.openMap();
+             }
+           },
+           { text: 'Cancel', role: 'cancel' }
+         ]
+      })
+       .then(actionSheetEl => {
+         actionSheetEl.present();
+       });
+   }
 
-      });
-      modalEl.present();
-    });
+   private locateUser() {
+     if (!Capacitor.isPluginAvailable('Geolocation')) {
+       this.showErrorAlert();
+       return;
+     }
+     this.isLoading = true;
+     Plugins.Geolocation.getCurrentPosition()
+       .then(geoPosition => {
+         const coordinates: Coordinates = {
+           lat: geoPosition.coords.latitude,
+           lng: geoPosition.coords.longitude
+         };
+         this.createPlace(coordinates.lat, coordinates.lng);
+         this.isLoading = false;
+       })
+       .catch(err => {
+       this.isLoading = false;
+        this.showErrorAlert();
+       });
   }
+
+  private showErrorAlert() {
+     this.alertCtrl
+       .create({
+         header: 'Could not fetch location',
+         message: 'Please use the map to pick a location!',
+         buttons: ['Okay']
+       })
+       .then(alertEl => alertEl.present());
+  }
+
+private openMap() { 
+  this.modalCtrl.create({ component: MapModalComponent }).then(modalEl => {
+    modalEl.onDidDismiss().then(modalData => {
+      if (!modalData.data) {
+       // console.log(modalData.data);
+        return;
+      }
+      const coordinates: Coordinates = {
+        lat: modalData.data.lat,
+        lng: modalData.data.lng
+      };
+      this.createPlace(coordinates.lat, coordinates.lng);
+    });
+    modalEl.present();
+  });
+} 
+
+private createPlace(lat: number, lng: number) {
+  const pickedLocation: PlaceLocation = {
+    lat: lat,
+    lng: lng,
+    address: null,
+    staticMapImageUrl: null
+  };
+  this.isLoading = true;
+      this.getAddress(lat, lng)
+      .pipe(
+        switchMap(address => {
+          pickedLocation.address = address;
+          return of(
+            this.getMapImage(pickedLocation.lat, pickedLocation.lng, 14)
+          );
+        })
+        )
+        .subscribe(staticMapImageUrl => {
+          pickedLocation.staticMapImageUrl = staticMapImageUrl;
+          this.selectedLocationImage = staticMapImageUrl;
+          this.isLoading = false;
+          this.locationPick.emit(pickedLocation);
+        });
+}
 
    private getAddress(lat: number, lng: number) {
     return this.http
@@ -79,7 +146,7 @@ export class LocationPickerComponent implements OnInit {
        );   
    }
    private getMapImage(lat: number, lng: number, zoom: number) {
-    return `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=${zoom}&size=400x400&maptype=roadmap
+    return `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=${zoom}&size=500x300&maptype=roadmap
     &markers=color:red%7Clabel:Place%7C${lat},${lng}
     &key=${environment.googleMapsAPIKey}`;
   }
